bbbf74e41bd5e74428d0b654627bb728935a18fc
brun0be <bruno.berreby@gmail.com>
2023-02-20 22:41:00 +0200

    Next step.

---
+++ Score.py
@@ -0,0 +1,7 @@
+def add_score():
+    score_file = open("scores.txt", "a+")
+    score_file.readline()
+
+    score_file.write("\ntorah")
+    score_file.readline()
+
---
+++ Utils.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+SCORES_FILE_NAME = "./scores.txt"
+BAD_RETURN_CODE = 999
+
+
+def screen_cleaner():
+    os.system('clear')
