33e16f8b71de54c6ca3dea7c6ef11c7f2e07f9a0
brun0be <bruno.berreby@gmail.com>
2023-04-13 16:20:10 +0300

    Added last features.

--- MainScores.py
+++ MainScores.py
@@ -5,15 +5,16 @@ app = Flask(__name__, template_folder='templateFiles', static_folder='staticFile
 filename = Utils.SCORES_FILE_NAME
 error_code = Utils.BAD_RETURN_CODE
 
+
 @app.route('/')
 def index():
     try:
         score_file = open(filename, "r+")
         current_score = score_file.readline()
-        return render_template('index.html', score=current_score)
+        return render_template('index.html', SCORE=current_score)
     except (OSError, Exception):
-        return render_template('error.html'), error_code
+        return render_template('error.html', ERROR_CODE=error_code)
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', debug=True, port=30000)
+    app.run('0.0.0.0', debug=True, port=5000)
